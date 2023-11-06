329aae7a12e80c25864e404987bc6dfe1c69d93c
Sangtae Seo <126737931+SeoSangtae001027@users.noreply.github.com>
2023-11-06 23:37:44 +0900

    [Feat] #14 - 로그인 기능 구현

--- src/pages/signInPage/signIn.js
+++ src/pages/signInPage/signIn.js
@@ -1,7 +1,44 @@
+import React, { useState } from 'react';
 import '../../styles/signIn.scss';
 import Wave from '../../assets/images/wave.png';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function SignIn() {
+    const [formData, setFormData] = useState({
+        id: '',
+        password: '',
+    });
+
+    const navigate = useNavigate();
+
+    const handleInputChange = e => {
+        const { id, value } = e.target;
+        setFormData({
+            ...formData,
+            [id]: value,
+        });
+    };
+
+    const handleLogin = () => {
+        axios
+            .post('/api/login', formData)
+            .then(response => {
+                if (response.data.success) {
+                    navigate('/myPage');
+                } else {
+                    console.log('로그인 실패');
+                }
+            })
+            .catch(error => {
+                console.error('로그인 오류:', error);
+            });
+    };
+
+    const handleSignUp = () => {
+        navigate('/signUp');
+    };
+
     return (
         <div className="login-frame">
             <div className="login-box">
@@ -13,15 +50,27 @@ function SignIn() {
                     <div className="id-password-login-signup">
                         <div className="id-password">
                             <div className="id-box">
-                                <div className="id">id</div>
+                                <input
+                                    type="text"
+                                    id="id"
+                                    placeholder="Enter your ID"
+                                />
                             </div>
                             <div className="password-box">
-                                <div className="password">password</div>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    placeholder="Enter your password"
+                                />
                             </div>
                         </div>
                         <div className="login-signup">
-                            <div className="login">Log in</div>
-                            <div className="signup">Sign up</div>
+                            <div className="login" onClick={handleLogin}>
+                                Log in
+                            </div>
+                            <div className="signup" onClick={handleSignUp}>
+                                Sign up
+                            </div>
                         </div>
                     </div>
                 </div>
