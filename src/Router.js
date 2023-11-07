868dff6432777ce3e8ac66d7a2bf3e51d09916c1
dandamdandam <glue0440@gmail.com>
2023-11-07 12:52:41 +0900

    [Fix] merge 하면서 나온 오류 수정

--- src/Router.js
+++ src/Router.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from './pages/signInPage/signIn';
 import SignUp from './pages/signUpPage/signUp';
 import Main from './pages/mainPage/main';
-import BoardList from './pages/boardPage/boardList';
+import BoardList from './pages/boardPage/BoardList';
 import BoardDetail from './pages/boardDetailPage/BoardDetail';
 import MyPage from './pages/myPage/myPage';
 import NewPost from './pages/newPostPage/newPost';
